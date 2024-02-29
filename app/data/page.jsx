fb2edf2d0791d85ca4f6f7f1bb1677ad577f5ce5
pynrctlg <tlg.pynrc@gmail.com>
2024-02-29 13:17:40 +0300

    update

---
+++ app/data/page.jsx
@@ -0,0 +1,12 @@
+import Box from "@/components/Box";
+import Header from "@/components/Header";
+import Iframe from "@/components/Iframe";
+export default function Data() {
+    return (
+        <div>
+            <Header />
+            <Iframe />
+            <Box />
+        </div>
+    )
+}
--- app/page.js
+++ app/page.js
@@ -1,13 +1,47 @@
-import Box from "@/components/Box";
-import Header from "@/components/Header";
-import Iframe from "@/components/Iframe";
-
+import logo from "@/public/logo.jpg";
+import Image from "next/image";
 export default function Home() {
   return (
-    <main>
-      <Header />
-      <Iframe />
-        <Box />
-    </main>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="bg-white px-28 py-10 rounded-2xl shadow-md">
+        <div><Image src={logo} className="m-auto" alt="buzkar"  width={200} height={88}/></div>
+        <form class="mt-6" action="#" method="POST">
+          <div>
+            <label class="block text-gray-700">Email Address</label>
+            <input
+              type="email"
+              name=""
+              id=""
+              placeholder="Enter Email Address"
+              class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500 focus:bg-white focus:outline-none"
+              autofocus
+              autocomplete
+              required
+            />
+          </div>
+
+          <div class="mt-4">
+            <label class="block text-gray-700">Password</label>
+            <input
+              type="password"
+              name=""
+              id=""
+              placeholder="Enter Password"
+              minlength="6"
+              class="w-full px-4 py-3 rounded-lg bg-gray-200 mt-2 border focus:border-blue-500
+                  focus:bg-white focus:outline-none"
+              required
+            />
+          </div>
+          <button
+            type="submit"
+            class="w-full block bg-blue-500 hover:bg-blue-400 focus:bg-blue-400 text-white font-semibold rounded-lg
+                px-4 py-3 mt-6"
+          >
+            Log In
+          </button>
+        </form>
+      </div>
+    </div>
   );
 }

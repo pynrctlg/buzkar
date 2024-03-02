380000e2ee497b8c19b4ee5d6803b68b5b913068
pynrctlg <tlg.pynrc@gmail.com>
2024-03-02 14:49:25 +0300

    asd

--- components/Iframe.jsx
+++ components/Iframe.jsx
@@ -1,7 +1,7 @@
 import { IoCloseCircle } from "react-icons/io5";
 export default function Iframe({ iframeDiv, setiframeDiv }) {
     return (
-        <div className={`max-w-[800px] lg:w-full m-auto mt-8 w-[90%] relative ${iframeDiv === true ? 'block' : 'hidden'}`}>
+        <div className={`lg:w-full m-auto mt-8 w-[320px] h-[256px] relative ${iframeDiv === true ? 'block' : 'hidden'}`}>
             <IoCloseCircle className="absolute top-0 right-0 z-10 text-white text-3xl cursor-pointer" onClick={() => (setiframeDiv(false))} />
             {
                 iframeDiv === true && <iframe className="w-[320px] h-[256px] m-auto rounded-xl" src="https://kamera.buzkar.keenetic.pro" ></iframe>

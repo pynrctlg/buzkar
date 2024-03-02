e9aaa8ce0929e183df73802da942d5c2fdaaebbc
pynrctlg <tlg.pynrc@gmail.com>
2024-03-02 14:47:37 +0300

    asa

--- components/Iframe.jsx
+++ components/Iframe.jsx
@@ -4,7 +4,7 @@ export default function Iframe({ iframeDiv, setiframeDiv }) {
         <div className={`max-w-[800px] lg:w-full m-auto mt-8 w-[90%] relative ${iframeDiv === true ? 'block' : 'hidden'}`}>
             <IoCloseCircle className="absolute top-0 right-0 z-10 text-white text-3xl cursor-pointer" onClick={() => (setiframeDiv(false))} />
             {
-                iframeDiv === true && <iframe className="w-full h-[300px] rounded-xl" src="https://kamera.buzkar.keenetic.pro" ></iframe>
+                iframeDiv === true && <iframe className="w-[320px] h-[256px] m-auto rounded-xl" src="https://kamera.buzkar.keenetic.pro" ></iframe>
             }
             
 

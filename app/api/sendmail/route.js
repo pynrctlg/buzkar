8754ca7abc79d85aaec39e8bfd2159432207b5ae
Bircan Tanoba <-get>
2024-03-02 15:05:56 +0300

    .

--- app/api/sendmail/route.js
+++ app/api/sendmail/route.js
@@ -1,5 +1,12 @@
 import { NextResponse, NextRequest } from "next/server";
 import nodemailer from "nodemailer"
+import axios from "axios";
+
+const apiEndpoint = "https://www:@rts.buzkar.keenetic.pro";
+
+const http = axios.create({
+    baseURL: apiEndpoint,
+})
 
 // Replace with your SMTP credentials
 const smtpOptions = {
@@ -23,10 +30,12 @@ const sendEmail = async (data) => {
     })
 }
 
-export async function POST(req) {
-    const body = await req.json();
+export async function GET(req) {
 
-    const msg = JSON.stringify(body);
+    const res = await http.post('/get-data');
+    const data = res.data;
+
+    const msg = JSON.stringify(data);
 
     let senData = {
         to: "bircantanoba@gmail.com",
@@ -38,3 +47,18 @@ export async function POST(req) {
 
     return NextResponse.json({ message: msg }, { status: 200 })
 }
+// export async function POST(req) {
+//     const body = await req.json();
+
+//     const msg = JSON.stringify(body);
+
+//     let senData = {
+//         to: "bircantanoba@gmail.com",
+//         subject: "Buzkar Bilgi",
+//         html: msg,
+//     };
+
+//     await sendEmail(senData);
+
+//     return NextResponse.json({ message: msg }, { status: 200 })
+// }

6d0495df982d34571a7f20e9a39cca0045680585
Bircan Tanoba <bircantanoba@gmail.com>
2024-12-19 10:35:47 +0300

    .

---
+++ app/rana/page.jsx
@@ -0,0 +1,11 @@
+import Box from "@/components/Box";
+import Header from "@/components/Header";
+
+export default function Data() {
+    return (
+        <div>
+            <Header />
+            <center style={{"padding":"50px"}}>Çok yakında babası</center>
+        </div>
+    )
+}
